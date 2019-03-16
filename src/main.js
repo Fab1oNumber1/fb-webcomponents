fa9a277c99eb195dc406ea83dd7054a70c19ea78
Fab1oNumber1 <fabio.bsser@hotmail.com>
2019-03-16 22:25:56 +0100

    require context

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import Vue from "vue";
 import App from "./App.vue";
+import requireContext from "require-context";
 
 Vue.config.productionTip = false;
 Vue.config.devtools = false;
+
+const files = require("./components/FbInput.vue");
+console.log(require);
 
 new Vue({
   render: h => h(App)
